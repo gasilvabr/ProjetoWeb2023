f9b4801b8fa97b62be4c0fcab748021da91f8549
George Azevedo da Silva <gasilvabr@hotmail.com>
2023-05-08 14:45:19 -0300

    Finalizando o cap3

--- WebAppProjeto2023G1/WebAppProjeto2023G1/Controllers/FabricantesController.cs
+++ WebAppProjeto2023G1/WebAppProjeto2023G1/Controllers/FabricantesController.cs
@@ -103,6 +103,7 @@ namespace WebAppProjeto2023G1.Controllers
             Fabricante fabricante = context.Fabricantes.Find(id);
             context.Fabricantes.Remove(fabricante);
             context.SaveChanges();
+            TempData["Message"] = "Fabricante \"" + fabricante.Nome.ToUpper() + "\" foi removido";
             return RedirectToAction("Index");
         }
 
