20723f42461b8f7d305d947686cdae18b59a2d5e
George Azevedo da Silva <gasilvabr@hotmail.com>
2023-06-05 14:33:39 -0300

    Details e Edit

--- WebAppProjeto2023G1/WebAppProjeto2023G1/Controllers/ProdutosController.cs
+++ WebAppProjeto2023G1/WebAppProjeto2023G1/Controllers/ProdutosController.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using WebAppProjeto2023G1.Models;
 using System.Data.Entity;
+using System.Net;
 
 namespace WebAppProjeto2023G1.Controllers
 {
@@ -19,9 +20,18 @@ namespace WebAppProjeto2023G1.Controllers
         }
 
         // GET: Produtos/Details/5
-        public ActionResult Details(int id)
+        public ActionResult Details(long? id)
         {
-            return View();
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Produto produto = context.Produtos.Where(p => p.ProdutoId == id).Include(c => c.Categoria).Include(f => f.Fabricante).First();
+            if (produto == null)
+            {
+                return HttpNotFound();
+            }
+            return View(produto);
         }
 
         // GET: Produtos/Create
@@ -52,19 +62,33 @@ namespace WebAppProjeto2023G1.Controllers
         }
 
         // GET: Produtos/Edit/5
-        public ActionResult Edit(int id)
+        public ActionResult Edit(long? id)
         {
-            return View();
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Produto produto = context.Produtos.Find(id);
+            if (produto == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CategoriaId = new SelectList(context.Categorias.OrderBy(b => b.Nome), "CategoriaId",
+            "Nome", produto.CategoriaId);
+            ViewBag.FabricanteId = new SelectList(context.Fabricantes.OrderBy(b => b.Nome), "FabricanteId",
+            "Nome", produto.FabricanteId);
+            return View(produto);
         }
 
         // POST: Produtos/Edit/5
         [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
+        public ActionResult Edit(Produto produto)
         {
             try
             {
                 // TODO: Add update logic here
-
+                context.Entry(produto).State = EntityState.Modified;
+                context.SaveChanges();
                 return RedirectToAction("Index");
             }
             catch
