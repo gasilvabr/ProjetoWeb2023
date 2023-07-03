a8172333cae4dc0d83ff5bb3ca360c5b76bffd04
George Azevedo da Silva <gasilvabr@hotmail.com>
2023-07-03 15:53:01 -0300

    ajuste camada modelo

--- WebAppProjeto2023G2/Modelo/Tabelas/Categoria.cs
+++ WebAppProjeto2023G2/Modelo/Tabelas/Categoria.cs
@@ -1,9 +1,10 @@
-﻿using System;
+﻿using Modelo.Cadastros;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace WebAppProjeto2023G2.Models
+namespace Modelo.Tabelas
 {
     public class Categoria
     {
