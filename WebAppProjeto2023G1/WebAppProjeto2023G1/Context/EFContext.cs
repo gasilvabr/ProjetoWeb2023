45f9ddec79a171ea3c2a539793eec40c5eceed75
George Azevedo da Silva <gasilvabr@hotmail.com>
2023-08-28 15:57:24 -0300

    Cap 06 Anotações

--- WebAppProjeto2023G1/WebAppProjeto2023G1/Context/EFContext.cs
+++ WebAppProjeto2023G1/WebAppProjeto2023G1/Context/EFContext.cs
@@ -3,8 +3,10 @@ using Modelo.Tabelas;
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
 using System.Linq;
 using System.Web;
+using WebAppProjeto2023G1.Migrations;
 
 namespace WebAppProjeto2023G1.Models
 {
@@ -12,11 +14,20 @@ namespace WebAppProjeto2023G1.Models
     {
         public EFContext() : base("Asp_Net_MVC_CS")
         {
-            Database.SetInitializer<EFContext>(
-            new DropCreateDatabaseIfModelChanges<EFContext>());
+            //Database.SetInitializer<EFContext>(
+            //new DropCreateDatabaseIfModelChanges<EFContext>());
+            Database.SetInitializer<EFContext>(new
+              MigrateDatabaseToLatestVersion<EFContext, Configuration>());
+        }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
         }
         public DbSet<Categoria> Categorias { get; set; }
         public DbSet<Fabricante> Fabricantes { get; set; }
         public DbSet<Produto> Produtos { get; set; }
     }
+
+
 }
--- WebAppProjeto2023G2/WebAppProjeto2023G2/Context/EFContext.cs
+++ WebAppProjeto2023G2/WebAppProjeto2023G2/Context/EFContext.cs
@@ -3,8 +3,10 @@ using Modelo.Tabelas;
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
 using System.Linq;
 using System.Web;
+using WebAppProjeto2023G2.Migrations;
 
 namespace WebAppProjeto2023G2.Models
 {
@@ -12,11 +14,19 @@ namespace WebAppProjeto2023G2.Models
     {
         public EFContext() : base("Asp_Net_MVC_CS")
         {
-            Database.SetInitializer<EFContext>(
-            new DropCreateDatabaseIfModelChanges<EFContext>());
+            //Database.SetInitializer<EFContext>(
+            //new DropCreateDatabaseIfModelChanges<EFContext>());
+            Database.SetInitializer<EFContext>(new 
+              MigrateDatabaseToLatestVersion<EFContext, Configuration>());
         }
         public DbSet<Categoria> Categorias { get; set; }
         public DbSet<Fabricante> Fabricantes { get; set; }
         public DbSet<Produto> Produtos { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            //base.OnModelCreating(modelBuilder);
+            //  modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
     }
 }
