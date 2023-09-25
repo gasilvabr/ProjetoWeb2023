256fdb6b1eb1cc2334b236f36caa6d348fc1f54e
George Azevedo da Silva <gasilvabr@hotmail.com>
2023-09-25 14:28:38 -0300

    inclusao da autenticação

---
+++ WebAppProjeto2023G1/WebAppProjeto2023G1/App_Start/IdentityConfig.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebAppProjeto2023G1.DAL;
+using WebAppProjeto2023G1.Infraestrutura;
+
+
+namespace WebAppProjeto2023G1.App_Start
+{
+    public class IdentityConfig
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.CreatePerOwinContext<IdentityDbContextAplicacao>
+            (IdentityDbContextAplicacao.Create);
+            app.CreatePerOwinContext<GerenciadorUsuario>(GerenciadorUsuario.Create);
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/Seguranca/Account/Login"),
+            });
+        }
+    }
+}
