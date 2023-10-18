ede3e252dd7bb7cc4e0b4b1d7479c04a63c1a18c
George Azevedo da Silva <gasilvabr@hotmail.com>
2023-10-18 11:54:31 -0300

    Carrinho DropDownList

--- WebAppProjeto2023G4/WebAppProjeto2023G2/Areas/Carrinho/Controllers/CarrinhosController.cs
+++ WebAppProjeto2023G4/WebAppProjeto2023G2/Areas/Carrinho/Controllers/CarrinhosController.cs
@@ -21,25 +21,33 @@ namespace WebAppProjeto2023G2.Areas.Carrinho.Controllers
 				carrinho = new List<ItemCarrinho>();
 				HttpContext.Session["carrinho"] = carrinho;
 			}
+			PopularViewBag();
 			return View(carrinho);
 		}
 
 		[HttpPost]
 		[ValidateAntiForgeryToken]
-		public PartialViewResult AddProduto(FormCollection collection)
+		public ActionResult AddProduto(FormCollection collection)
 		{
 			List<ItemCarrinho> carrinho = HttpContext.Session["carrinho"] as List<ItemCarrinho>;
-			var produto = produtoServico.ObterProdutoPorId(Convert.ToInt32(collection.Get("idproduto")));
+			var produto2 = produtoServico.ObterProdutoPorId(Convert.ToInt32(collection.Get("ItemCarrinhoId")));
+			var qtd = Convert.ToInt32(collection.Get("Quantidade"));
+			//var produto = produtoServico.ObterProdutoPorId(Convert.ToInt32(collection.Get("idproduto")));
 			var itemCarrinho = new ItemCarrinho()
 			{
-				Produto = produto,
-				Quantidade = 1,
+				Produto = produto2,
+				Quantidade = qtd,
 				ValorUnitario = 1 // produto.ValorUnitario
 			};
 			carrinho.Add(itemCarrinho);
 			HttpContext.Session["carrinho"] = carrinho;
-			return PartialView("_ItensCarrinho", carrinho);
+			//return PartialView("_ItensCarrinho", carrinho);
+			return RedirectToAction("Create");
 		}
 
+		private void PopularViewBag()
+		{
+			ViewBag.ItemCarrinhoId = new SelectList(produtoServico.ObterProdutosClassificadosPorNome(),"ProdutoId", "Nome");
+		}
 	}
 }
