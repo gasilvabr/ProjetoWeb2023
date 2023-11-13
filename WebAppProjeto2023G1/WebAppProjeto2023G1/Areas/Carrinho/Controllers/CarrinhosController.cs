bfd59b6fd0df8c80e63f4c8ee6005c2991057892
George Azevedo da Silva <gasilvabr@hotmail.com>
2023-11-13 13:17:57 -0300

    carrinho

---
+++ WebAppProjeto2023G1/WebAppProjeto2023G1/Areas/Carrinho/Controllers/CarrinhosController.cs
@@ -0,0 +1,53 @@
+﻿using Modelo.Carrinho;
+using Servico.Cadastros;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebAppProjeto2023G1.Areas.Carrinho.Controllers
+{
+    public class CarrinhosController : Controller
+    {
+        ProdutoServico produtoServico = new ProdutoServico();
+
+        private void PopularViewBag()
+        {
+            ViewBag.ItemCarrinhoId = new
+              SelectList(produtoServico.ObterProdutosClassificadosPorNome(),
+            "ProdutoId", "Nome");
+        }
+        public ActionResult Create()
+        {
+            IEnumerable<ItemCarrinho> carrinho =
+            HttpContext.Session["carrinho"] as IEnumerable<ItemCarrinho>;
+            if (carrinho == null)
+            {
+                carrinho = new List<ItemCarrinho>();
+                HttpContext.Session["carrinho"] = carrinho;
+            }
+            PopularViewBag();
+            return View(carrinho);
+        }
+
+        public ActionResult AddProduto(FormCollection collection)
+        {
+            List<ItemCarrinho> carrinho = HttpContext.Session["carrinho"] as
+              List<ItemCarrinho>;
+            var produto2 = produtoServico.ObterProdutoPorId(
+              Convert.ToInt32(collection.Get("ItemCarrinhoId")));
+            var qtd = Convert.ToInt32(collection.Get("Quantidade"));
+            var itemCarrinho = new ItemCarrinho()
+            {
+                Produto = produto2,
+                Quantidade = qtd,
+                ValorUnitario = 1
+            };
+            carrinho.Add(itemCarrinho);
+            HttpContext.Session["carrinho"] = carrinho;
+            return RedirectToAction("Create");
+        }
+
+    }
+}
